689b9a941bfae55916b41e7a758308c87afa549e
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-03 14:01:57 +0000

    Initial react routes added to App.js. Components AddContact and Contacts boiler created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Contacts from './Components/Contacts/Contacts';
+import AddContacts from './Components/AddContacts/AddContacts';
+
 function App() {
   return (
-    <div>
-      <h1>Contacts</h1>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Contacts} />
+          <Route path="/addContacts" exact component={AddContacts} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
