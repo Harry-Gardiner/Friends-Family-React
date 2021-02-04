0df4aa39d3c6ac7420304add709663291cb4bc6c
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-04 11:19:57 +0000

    Local state added using useState hook

--- src/Components/AddContacts/AddContacts.js
+++ src/Components/AddContacts/AddContacts.js
@@ -1,24 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddContacts = () => {
+    // local state
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        dob: "",
+    });
+
+
+    // Events
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setState({
+            ...state,
+            [e.target.name]: value,
+        });
+    };
 
     return (
         <form>
             <div className="form-group mt-5">
                 <label htmlFor="full name">Full Name:</label>
-                <input type="text" className="form-control" id="full name" name="full name" required />
+                <input
+                    type="text"
+                    className="form-control"
+                    id="full name"
+                    name="name"
+                    required
+                    onChange={handleChange}
+                    value={state.name}
+                />
             </div>
             <div className="form-group">
                 <label htmlFor="Email">Email:</label>
-                <input type="email" className="form-control" id="Email" name="Email" required />
+                <input
+                    type="email"
+                    className="form-control"
+                    id="Email"
+                    name="email"
+                    required
+                    onChange={handleChange}
+                    value={state.email}
+                />
             </div>
             <div className="form-group">
                 <label htmlFor="Email">Phone Number:</label>
-                <input type="text" className="form-control" id="Phone" name="Phone" required />
+                <input
+                    type="text"
+                    className="form-control"
+                    id="Phone"
+                    name="phone"
+                    required
+                    onChange={handleChange}
+                    value={state.phone}
+                />
             </div>
             <div className="form-group">
                 <label htmlFor="Email">Date Of Birth:</label>
-                <input type="email" className="form-control" id="Date Of Birth" name="Date Of Birth" required />
+                <input
+                    type="date"
+                    className="form-control"
+                    id="Date Of Birth"
+                    name="dob"
+                    required
+                    onChange={handleChange}
+                    value={state.dob}
+                />
             </div>
             <button className="btn btn-primary" type="submit">Submit form</button>
         </form>
