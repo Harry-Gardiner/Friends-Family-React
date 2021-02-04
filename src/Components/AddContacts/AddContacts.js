491fc68634f9d069448d287bd5243f52406c6235
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-04 11:02:03 +0000

    required validation added to inputs

--- src/Components/AddContacts/AddContacts.js
+++ src/Components/AddContacts/AddContacts.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
 const AddContacts = () => {
+
     return (
         <form>
             <div className="form-group mt-5">
-                <label htmlFor="full name">Full Name</label>
-                <input type="text" className="form-control" id="full name" name="full name" />
+                <label htmlFor="full name">Full Name:</label>
+                <input type="text" className="form-control" id="full name" name="full name" required />
             </div>
             <div className="form-group">
-                <label htmlFor="Email">Email</label>
-                <input type="email" className="form-control" id="Email" name="Email" />
+                <label htmlFor="Email">Email:</label>
+                <input type="email" className="form-control" id="Email" name="Email" required />
             </div>
             <div className="form-group">
-                <label htmlFor="Email">Phone Number</label>
-                <input type="text" className="form-control" id="Phone" name="Phone" />
+                <label htmlFor="Email">Phone Number:</label>
+                <input type="text" className="form-control" id="Phone" name="Phone" required />
             </div>
             <div className="form-group">
-                <label htmlFor="Email">Date Of Birth</label>
-                <input type="email" className="form-control" id="Date Of Birth" name="Date Of Birth" />
+                <label htmlFor="Email">Date Of Birth:</label>
+                <input type="email" className="form-control" id="Date Of Birth" name="Date Of Birth" required />
             </div>
-            <button class="btn btn-primary" type="submit">Submit form</button>
+            <button className="btn btn-primary" type="submit">Submit form</button>
         </form>
     );
 };
