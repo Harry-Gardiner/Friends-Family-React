c503328217c4ed042045ef6af0a2f0171bf4154e
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-04 10:30:54 +0000

    Form inputs added

--- src/Components/AddContacts/AddContacts.js
+++ src/Components/AddContacts/AddContacts.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 const AddContacts = () => {
     return (
-        <div>
-            <h1>Add Contacts</h1>
-        </div>
+        <form>
+            <div className="form-group">
+                <label for="full name">Full Name</label>
+                <input type="text" className="form-control" id="full name" name="full name" />
+            </div>
+            <div className="form-group">
+                <label for="Email">Email</label>
+                <input type="email" className="form-control" id="Email" name="Email" />
+            </div>
+            <div className="form-group">
+                <label for="Email">Phone Number</label>
+                <input type="text" className="form-control" id="Phone" name="Phone" />
+            </div>
+            <div className="form-group">
+                <label for="Email">Date Of Birth</label>
+                <input type="email" className="form-control" id="Date Of Birth" name="Date Of Birth" />
+            </div>
+        </form>
     );
 };
 
