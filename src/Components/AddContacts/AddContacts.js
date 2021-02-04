b50c3f7ff0669a4b8ed5b0910cf2c30aa65ef8a0
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-04 10:34:48 +0000

    Submit button added

--- src/Components/AddContacts/AddContacts.js
+++ src/Components/AddContacts/AddContacts.js
@@ -3,22 +3,23 @@ import React from 'react';
 const AddContacts = () => {
     return (
         <form>
-            <div className="form-group">
-                <label for="full name">Full Name</label>
+            <div className="form-group mt-5">
+                <label htmlFor="full name">Full Name</label>
                 <input type="text" className="form-control" id="full name" name="full name" />
             </div>
             <div className="form-group">
-                <label for="Email">Email</label>
+                <label htmlFor="Email">Email</label>
                 <input type="email" className="form-control" id="Email" name="Email" />
             </div>
             <div className="form-group">
-                <label for="Email">Phone Number</label>
+                <label htmlFor="Email">Phone Number</label>
                 <input type="text" className="form-control" id="Phone" name="Phone" />
             </div>
             <div className="form-group">
-                <label for="Email">Date Of Birth</label>
+                <label htmlFor="Email">Date Of Birth</label>
                 <input type="email" className="form-control" id="Date Of Birth" name="Date Of Birth" />
             </div>
+            <button class="btn btn-primary" type="submit">Submit form</button>
         </form>
     );
 };
