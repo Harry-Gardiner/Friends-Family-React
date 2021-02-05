a4d8f23fbe31367ff24f444b7d1779cd5291f24b
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-05 11:15:34 +0000

    GET contacts request created. Conatcts are output in cards

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from '../../data/axios-config';
 
 const Contacts = () => {
+    const [contacts, setContacts] = useState([]);
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        if (!loaded) {
+            axios.get("contact").then(({ data }) => {
+                setContacts(data);
+                setLoaded(true);
+                console.log(contacts);
+            });
+        } else {
+            return null
+        }
+
+    }, [loaded, contacts]);
+
     return (
         <div>
-            <h1>Contacts</h1>
+            <div>
+                {contacts.map((contact) => (
+                    <div className="card m-4" key={contact.full_name}>
+                        <div className="card-body">
+                            <div className="row lineHeight">
+                                <div className="col-md-2">
+                                    <div className="firstLetter">{contact.full_name[0]}</div>
+                                </div>
+                                <div className="col-md-2">{contact.full_name}</div>
+                                <div className="col-md-4">{contact.email}</div>
+                                <div className="col-md-2">{contact.phone}</div>
+                                <div className="col-md-2">{contact.date_of_birth}</div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
